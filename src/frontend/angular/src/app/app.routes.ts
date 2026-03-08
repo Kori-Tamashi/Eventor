821e9eb34dadd8d9282a9d891604872302e9d6b6
AndresRojas1512 <andresrojasfcc@gmail.com>
2026-03-08 14:55:32 +0300

    (+) Создание страниц и настройка routing

--- src/frontend/angular/src/app/app.routes.ts
+++ src/frontend/angular/src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./pages/auth/auth').then((m) => m.Auth),
+  },
+  {
+    path: 'app',
+    loadComponent: () =>
+      import('./layout/shell/shell').then((m) => m.Shell),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./pages/dashboard/dashboard').then((m) => m.Dashboard)
+      },
+      {
+        path: 'events',
+        loadComponent: () => import('./pages/events/events').then((m) => m.Events)
+      },
+      {
+        path: 'organization',
+        loadComponent: () => import('./pages/organization/organization').then((m) => m.Organization)
+      },
+      {
+        path: 'profile',
+        loadComponent: () => import('./pages/profile/profile').then((m) => m.Profile)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
