0a85636dc1ce5aa964d5b4dbf13fbb7dae060158
AndresRojas1512 <andresrojasfcc@gmail.com>
2026-03-13 02:27:21 +0300

    Добавление standalone во все файлы

--- src/frontend/angular/src/app/pages/events/events.ts
+++ src/frontend/angular/src/app/pages/events/events.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-events',
+  standalone: true,
   imports: [],
   templateUrl: './events.html',
   styleUrl: './events.scss',
