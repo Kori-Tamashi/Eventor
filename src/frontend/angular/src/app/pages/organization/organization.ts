8b033d0b6e388ce9f84dd069111d00e0b08ff0f2
AndresRojas1512 <andresrojasfcc@gmail.com>
2026-03-20 17:32:06 +0300

    (+) Добавлены глобальные стили и применены к компоненту sidebar

--- src/frontend/angular/src/app/pages/organization/organization.ts
+++ src/frontend/angular/src/app/pages/organization/organization.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-organization',
+  standalone: true,
   imports: [],
   templateUrl: './organization.html',
   styleUrl: './organization.scss',
