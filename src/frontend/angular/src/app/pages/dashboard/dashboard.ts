0b5d501d54c68c74c53b2f7dc691180ddcabb5d9
AndresRojas1512 <andresrojasfcc@gmail.com>
2026-03-23 18:09:47 +0300

    (+) Подготовлен шаблон страницы 'Dashboard'

--- src/frontend/angular/src/app/pages/dashboard/dashboard.ts
+++ src/frontend/angular/src/app/pages/dashboard/dashboard.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import {Button, ButtonModule} from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+type DashboardRow = {
+  name: string;
+  description: string;
+  date: string;
+  rating: number;
+}
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, ButtonModule, TableModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+
+  readonly pageSize = signal<number>(10);
+
+  readonly pageIndex = signal<number>(0);
+
+  readonly allRows = signal<DashboardRow[]>([
+    { name: 'Название', description: 'Описание', date: 'dd.mm.yyyy', rating: 0 },
+    { name: 'Название', description: 'Описание', date: 'dd.mm.yyyy', rating: 0 },
+    { name: 'Название', description: 'Описание', date: 'dd.mm.yyyy', rating: 0 },
+    { name: 'Название', description: 'Описание', date: 'dd.mm.yyyy', rating: 0 },
+    { name: 'Название', description: 'Описание', date: 'dd.mm.yyyy', rating: 0 },
+    { name: 'Название', description: 'Описание', date: 'dd.mm.yyyy', rating: 0 },
+    { name: 'Название', description: 'Описание', date: 'dd.mm.yyyy', rating: 0 },
+    { name: 'Название', description: 'Описание', date: 'dd.mm.yyyy', rating: 0 },
+    { name: 'Название', description: 'Описание', date: 'dd.mm.yyyy', rating: 0 },
+    { name: 'Название', description: 'Описание', date: 'dd.mm.yyyy', rating: 0 },
+    { name: 'Название', description: 'Описание', date: 'dd.mm.yyyy', rating: 0 },
+  ]);
+
+
+  readonly totalPages = computed(() => {
+    const total = this.allRows().length;
+    const size = this.pageSize();
+    return Math.max(1, Math.ceil(total / size));
+  });
+
+  readonly pageLabel = computed(() => `${this.pageIndex() + 1} из ${this.totalPages()}`);
+
+  readonly pagedRows = computed(() => {
+    const size = this.pageSize();
+    const start = this.pageIndex() * size;
+    return this.allRows().slice(start, start + size);
+  });
+
+  readonly canPrev = computed(() => this.pageIndex() > 0);
+  readonly canNext = computed(() => this.pageIndex() < this.totalPages() - 1);
+
+  prevPage(): void {
+    if (!this.canPrev())
+      return;
+    this.pageIndex.update((v) => v - 1);
+  }
+
+  nextPage(): void {
+    if (!this.canNext())
+      return;
+    this.pageIndex.update((v) => v + 1);
+  }
 
 }
