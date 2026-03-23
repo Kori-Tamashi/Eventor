fcf6623e21fed98455195c3d5bbade961a133a25
AndresRojas1512 <andresrojasfcc@gmail.com>
2026-03-23 15:06:29 +0300

    (+) Добавлена базовая структура таблицы

--- src/frontend/angular/src/app/pages/organization/organization.ts
+++ src/frontend/angular/src/app/pages/organization/organization.ts
@@ -1,14 +1,57 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+type OrganizationEventRow = {
+  name: string;
+  location: string;
+  description: string;
+  date: string;
+  peopleCount: number;
+  daysCount: number;
+  markup: number;
+  rating: number;
+}
 
 @Component({
   selector: 'app-organization',
   standalone: true,
-  imports: [InputTextModule, ButtonModule],
+  imports: [InputTextModule, ButtonModule, TableModule],
   templateUrl: './organization.html',
   styleUrl: './organization.scss',
 })
 export class Organization {
-
+  readonly rows: OrganizationEventRow[] = [
+    {
+      name: 'Название',
+      location: 'Локация',
+      description: 'Описание',
+      date: 'dd.mm.yyyy',
+      peopleCount: 0,
+      daysCount: 0,
+      markup: 0,
+      rating: 0,
+    },
+    {
+      name: 'Название',
+      location: 'Локация',
+      description: 'Описание',
+      date: 'dd.mm.yyyy',
+      peopleCount: 0,
+      daysCount: 0,
+      markup: 0,
+      rating: 0,
+    },
+    {
+      name: 'Название',
+      location: 'Локация',
+      description: 'Описание',
+      date: 'dd.mm.yyyy',
+      peopleCount: 0,
+      daysCount: 0,
+      markup: 0,
+      rating: 0,
+    },
+  ];
 }
