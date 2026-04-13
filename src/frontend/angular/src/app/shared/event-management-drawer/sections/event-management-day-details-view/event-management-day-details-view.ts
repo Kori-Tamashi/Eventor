bc48037581ae81aceca5e38740a5e521e5dca668
AndresRojas1512 <andresrojasfcc@gmail.com>
2026-04-13 13:37:41 +0300

    (+) Полностью разработан подраздел drawer'a для выбранного мероприятия

--- src/frontend/angular/src/app/shared/event-management-drawer/sections/event-management-day-details-view/event-management-day-details-view.ts
+++ src/frontend/angular/src/app/shared/event-management-drawer/sections/event-management-day-details-view/event-management-day-details-view.ts
@@ -1,13 +1,21 @@
 import { Component, input, output } from '@angular/core';
 import { TabsModule } from 'primeng/tabs';
 import { EventManagementDrawerDayDetailsTab } from '../../../../core/ui/event-management-drawer.store';
+import { EventManagementDayAnalyticsTab } from '../event-management-day-analytics-tab/event-management-day-analytics-tab';
+import { EventManagementDayMenuTab } from '../event-management-day-menu-tab/event-management-day-menu-tab';
 import { EventManagementDayParticipantsTab } from '../event-management-day-participants-tab/event-management-day-participants-tab';
 import { EventManagementDaySettingsTab } from '../event-management-day-settings-tab/event-management-day-settings-tab';
 
 @Component({
   selector: 'app-event-management-day-details-view',
   standalone: true,
-  imports: [TabsModule, EventManagementDaySettingsTab, EventManagementDayParticipantsTab],
+  imports: [
+    TabsModule,
+    EventManagementDaySettingsTab,
+    EventManagementDayParticipantsTab,
+    EventManagementDayMenuTab,
+    EventManagementDayAnalyticsTab,
+  ],
   templateUrl: './event-management-day-details-view.html',
   styleUrl: './event-management-day-details-view.scss',
 })
